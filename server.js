23458c6d8c0accb0a0271a5f82ff7d4c9ffc4777
gwondohhun <ehgns8202@gmail.com>
2026-05-22 12:26:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// API proxy endpoint
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.post('/api/ask', async (req, res) => {
   const { messages, system } = req.body;
   const apiKey = process.env.ANTHROPIC_API_KEY;
